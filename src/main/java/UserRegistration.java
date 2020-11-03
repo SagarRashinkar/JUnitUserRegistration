42ed147140cdf0e7be73f6343bfa0d6c2a7edf1f
Sagar <sagarrashinkar2@gmail.com>
2020-11-03 06:18:49 +0530

    ADDED: last name validation

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -2,4 +2,7 @@ public class UserRegistration {
     public boolean validateFName(String fname){
         return (fname.matches("^[A-Z][a-z]{2,}"));
     }
+    public boolean validateLName(String lname){
+        return (lname.matches("^[A-Z][a-z]{2,}"));
+    }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -16,4 +16,17 @@ public class UserRegistrationTest {
         boolean isValid = user.validateFName("sa");
         Assert.assertTrue(isValid);
     }
+    //test case for valid Last name
+    @Test
+    public void testLNameValid(){
+        UserRegistration user = new UserRegistration();
+        boolean isValid = user.validateLName("Rashinkar");
+        Assert.assertTrue(isValid);
+    }
+    @Test
+    public void testLNameWithInValidData(){
+        UserRegistration user = new UserRegistration();
+        boolean isValid = user.validateLName("rashinkar");
+        Assert.assertTrue(isValid);
+    }
 }
