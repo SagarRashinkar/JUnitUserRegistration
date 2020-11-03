56163d4e20a31f1c0abcbd5ad8e7f10e253ff268
Sagar <sagarrashinkar2@gmail.com>
2020-11-03 06:39:36 +0530

    ADDED: email validation

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -5,4 +5,8 @@ public class UserRegistration {
     public boolean validateLName(String lname){
         return (lname.matches("^[A-Z][a-z]{2,}"));
     }
+
+    public boolean validateEmail(String email){
+        return (email.matches("^[a-z][a-zA-Z0-9+_.-]+@[a-zA-Z0-9.]+(co|com|in|au)$"));
+    }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -23,10 +23,26 @@ public class UserRegistrationTest {
         boolean isValid = user.validateLName("Rashinkar");
         Assert.assertTrue(isValid);
     }
+    //test case for invalid last name
     @Test
     public void testLNameWithInValidData(){
         UserRegistration user = new UserRegistration();
         boolean isValid = user.validateLName("rashinkar");
         Assert.assertTrue(isValid);
     }
+    //test case for valid email
+    @Test
+    public void testValidEmail(){
+        UserRegistration user = new UserRegistration();
+        boolean isValid = user.validateEmail("abc@gmail.com");
+        Assert.assertTrue(isValid);
+    }
+    //test case for invalid email
+    @Test
+    public void testInvalidEmail(){
+        UserRegistration user = new UserRegistration();
+        boolean isValid = user.validateEmail(".abc@gmail.com");
+        Assert.assertTrue(isValid);
+    }
+
 }
